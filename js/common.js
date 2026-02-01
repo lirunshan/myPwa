65f327503ac4c04d3abb092589d6b6cdecaf73d9
lirunshan <114592044+lirunshan@users.noreply.github.com>
2026-02-01 23:12:51 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
-function clearAll() {
+function clearAll(localStorageKey) {
     if (!confirm('确定清空所有记录？')) return;
-    localStorage.removeItem(STORAGE_KEY);
+    localStorage.removeItem(localStorageKey);
     document.querySelectorAll('input[type=checkbox]')
       .forEach(cb => cb.checked = false);
     data = {};
